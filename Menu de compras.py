6473202ee01b712ed4efb7e1caf21e3609dd08e6
killer7luis <84632338+killer7luis@users.noreply.github.com>
2025-05-29 12:13:12 -0400

    Add files via upload

--- Menu de compras.py
+++ Menu de compras.py
@@ -55,12 +55,39 @@ while menuact == 1:
         compra = input("Por favor escriba el nombre del producto que quiera añadir a su lista: ")
         if compra == "LECHE":
             listcomp.append(objetos[0])
-            print(listcomp)
             menuact = 1
         elif compra == "ARROZ":
-            
-#    elif menu == "MOSTRAR":
+            listcomp.append(objetos[1])
+            menuact = 1
+        elif compra == "CARNE":
+            listcomp.append(objetos[2])
+            menuact = 1
+        elif compra == "PAPA":
+            listcomp.append(objetos[3])
+            menuact = 1
+        else:
+            menuact = 1
+    elif menu == "MOSTRAR":
+        print(listcomp)
 
-#    elif menu == "ELIMINAR":
+    elif menu == "ELIMINAR":
+        menuact = 0
+        print(listcomp)
+        borrar = input("Por favor coloque el nombre de el objeto que quiera eliminar: ")
+        if borrar == "LECHE":
+            listcomp.remove(("Leche", 50))
+            menuact = 1
+        elif borrar == "ARROZ":
+            listcomp.remove(("Arroz", 100))
+            menuact = 1
+        elif borrar == "CARNE":
+            listcomp.remove(("Carne", 200))
+            menuact = 1
+        elif borrar == "PAPA":
+            listcomp.remove(("Papa", 10))
+            menuact = 1
+        else:
+            menuact = 1
 
-#    elif menu == "TOTAL":
+    elif menu == "TOTAL":
+        tot = input()
